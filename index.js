7bdd424f262a866c78ffbc49b4670e874fcd5fcf
lsh <jetaime12121@gmail.com>
2023-05-19 02:37:21 +0900

    css 완료

--- index.js
+++ index.js
@@ -8,3 +8,13 @@ startBtn.addEventListener('click', () => {
      pHand.style.animation="shakePlayer 1s ease";
      cHand.style.animation="shakeComputer 1s ease";
 });
+
+//시작하기 눌렀을 때 
+function gameStart(){
+    //플레이어, 컴퓨터 스코어
+    let pScore = 0;
+    let cScore = 0;
+
+    
+
+};
